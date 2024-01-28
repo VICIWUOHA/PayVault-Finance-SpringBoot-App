f0b18a9868f30dd2eca9be8c07dbb999b2ace59c
VICIWUOHA <viciwuoha@gmail.com>
2024-01-28 23:47:50 +0100

    added dependency injection comments

--- src/main/java/vicmicroservices/payvault/PayVaultRepository.java
+++ src/main/java/vicmicroservices/payvault/PayVaultRepository.java
@@ -3,7 +3,8 @@ package vicmicroservices.payvault;
 import org.springframework.data.repository.CrudRepository;
 
 // This Repository handles access to our data persistence layer.
-// it would be injected into our Controller Layer
+// it would be injected into our Controller Layer using dependency injection
+// The Long specifies the datatype for our PayCard Id field
 //@Repository
 interface PayVaultRepository extends CrudRepository<PayCard, Long> {
 
