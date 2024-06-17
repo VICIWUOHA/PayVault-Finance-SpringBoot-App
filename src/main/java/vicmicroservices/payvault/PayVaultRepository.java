9c962c4da93c9ef23358226302f940fc9c15c76d
VICIWUOHA <viciwuoha@gmail.com>
2024-06-17 02:51:52 +0100

    enforce security to ensure users only see their paycards

--- src/main/java/vicmicroservices/payvault/PayVaultRepository.java
+++ src/main/java/vicmicroservices/payvault/PayVaultRepository.java
@@ -1,5 +1,8 @@
 package vicmicroservices.payvault;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -9,4 +12,9 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 //@Repository
 interface PayVaultRepository extends CrudRepository<PayCard, Long>, PagingAndSortingRepository<PayCard, Long> {
 
+//    @Query("SELECT p FROM pay_card p WHERE p.Id = :id and customer = :customer")
+//    manually specify query this way . eg; Java expects id cols to be lower case not Id.
+    PayCard findByIdAndCustomer(Long id, String customer);
+
+    Page<PayCard> findByCustomer(String customer, PageRequest pageRequest);
 }
