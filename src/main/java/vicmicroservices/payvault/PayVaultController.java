59d2b1c1b088e1d4a84ca3225c9d850cab847636
VICIWUOHA <viciwuoha@gmail.com>
2024-08-23 04:33:19 +0100

    Update Controller Logic

--- src/main/java/vicmicroservices/payvault/PayVaultController.java
+++ src/main/java/vicmicroservices/payvault/PayVaultController.java
@@ -37,66 +37,57 @@ class PayVaultController {
     @GetMapping("/{payCardId}")
     private ResponseEntity<PayCard> getPayCard(@PathVariable Long payCardId, Principal principal) {
         Optional<PayCard> optionalPayCard = Optional
-                        .ofNullable(payVaultRepository.findByIdAndCustomer(payCardId, principal.getName()));
+                .ofNullable(payVaultRepository.findByIdAndCustomer(payCardId, principal.getName()));
         // Use functional style expression on one line
         return optionalPayCard.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
 
-//        if(optionalPayCard.isPresent()){
-//            return ResponseEntity.ok(optionalPayCard.get());
-//        }else{
-//            return ResponseEntity.notFound().build();
-//        }
-// The logic below can be abstracted to be handled by the Repository to ensure the Single Responsibility Principle.
-//        if (payCardId < 10000L) {
-//            PayCard payCard = new PayCard(9L, 780.5);
-//            System.out.println("Valid PayCard ID in db.");
-//            return ResponseEntity.ok(payCard);
-//        } else {
-//            return ResponseEntity.notFound().build();
-//        }
+        // The logic below can be abstracted to be handled by the Repository to ensure the Single Responsibility Principle.
+        //        if (payCardId < 10000L) {
+        //            PayCard payCard = new PayCard(9L, 780.5);
+        //            System.out.println("Valid PayCard ID in db.");
+        //            return ResponseEntity.ok(payCard);
+        //        } else {
+        //            return ResponseEntity.notFound().build();
+        //        }
     }
 
     @PostMapping(path = "/create")
     private ResponseEntity<Void> createPayCard(@RequestBody PayCard newPayCardInfo, UriComponentsBuilder ucb, Principal principal) {
         // when requests are sent to the /create endpoint, we get the principal who made the request, save it and return the location
-        PayCard customerPayCard = new PayCard(null,newPayCardInfo.balance(),principal.getName());
+        PayCard customerPayCard = new PayCard(null, newPayCardInfo.balance(), principal.getName());
         PayCard savedPayCard = payVaultRepository.save(customerPayCard);
         URI locationOfSavedPayCard = ucb.path("/api/v1/paycards/{id}")
                 .buildAndExpand(savedPayCard.id())
                 .toUri();
         return ResponseEntity.created(locationOfSavedPayCard).build();
         //to create the URI from a sting use the URI.create method
-//        return ResponseEntity.created(URI.create("/paycards/id?")).build();
+        // return ResponseEntity.created(URI.create("/paycards/id?")).build();
 
     }
 
-//    @GetMapping("/list_paycards")
-//    private ResponseEntity<Iterable<PayCard>> findAll() {
-//        return ResponseEntity.ok(payVaultRepository.findAll());
-//    }
 
-    // Users should be able to have multiple virtualCards
+    // Users should be able to have multiple virtualCards and make a GET request to show them.
     @GetMapping("/list_paycards")
-    private ResponseEntity<List<PayCard>> getAllPayCards(Pageable pageable, Principal principal){
+    private ResponseEntity<List<PayCard>> getAllPayCards(Pageable pageable, Principal principal) {
         Page<PayCard> page = payVaultRepository.findByCustomer(
                 principal.getName(),
                 PageRequest.of(
                         pageable.getPageNumber(),
                         pageable.getPageSize(),
                         pageable.getSortOr(Sort.by(Sort.Direction.DESC, "balance")))
-                );
+        );
         return ResponseEntity.ok(page.getContent());
     }
 
     // Allow Updates on Existing PayCards
     @PutMapping("/{requestedPayCardId}")
-    private ResponseEntity<Void> putPayCard(@PathVariable Long requestedPayCardId, @RequestBody PayCard payCardUpdate, Principal principal){
+    private ResponseEntity<Void> putPayCard(@PathVariable Long requestedPayCardId, @RequestBody PayCard payCardUpdate, Principal principal) {
         // find the card by ID and Customer, update its values (in a new PayCard object) and save
 
         PayCard existingPayCard = findPayCard(requestedPayCardId, principal);
         // if we got no response send 404 to client
-        if (existingPayCard!= null) {
-            PayCard updatedPayCard = new PayCard(existingPayCard.id(),payCardUpdate.balance(),principal.getName());
+        if (existingPayCard != null) {
+            PayCard updatedPayCard = new PayCard(existingPayCard.id(), payCardUpdate.balance(), principal.getName());
             payVaultRepository.save(updatedPayCard);
 
             return ResponseEntity.noContent().build();
@@ -107,20 +98,21 @@ class PayVaultController {
     }
 
     @DeleteMapping("/{requestedId}")
-    private ResponseEntity<Void> deletePayCard(@PathVariable Long requestedId, Principal principal){
+    private ResponseEntity<Void> deletePayCard(@PathVariable Long requestedId, Principal principal) {
 
-        if (!payVaultRepository.existsByIdAndCustomer(requestedId, principal.getName())){
-            System.out.println("==>> No PayCard with id: "+ requestedId);
-            return ResponseEntity.notFound().build();
+        if (payVaultRepository.existsByIdAndCustomer(requestedId, principal.getName())) {
+            // use deleteById
+            payVaultRepository.deleteById(requestedId);
+            return ResponseEntity.noContent().build();
         }
-        // use deleteById
-        payVaultRepository.deleteById(requestedId);
-        return ResponseEntity.noContent().build();
+
+        System.out.println("==>> No PayCard with id: " + requestedId);
+        return ResponseEntity.notFound().build();
 
     }
 
     // Helper method to find payCard with repository.
-    private PayCard findPayCard(Long requestedId, Principal principal){
+    private PayCard findPayCard(Long requestedId, Principal principal) {
         return payVaultRepository.findByIdAndCustomer(requestedId, principal.getName());
     }
 
