12b0322e90b70cfc7e25d5c82d203c28170e14fd
VICIWUOHA <viciwuoha@gmail.com>
2024-08-23 03:38:14 +0100

    standardize tests

--- src/test/java/vicmicroservices/payvault/PayVaultApplicationTests.java
+++ src/test/java/vicmicroservices/payvault/PayVaultApplicationTests.java
@@ -24,11 +24,11 @@ class PayVaultApplicationTests {
 //    this rest template helps us make mock API requests to the locally running application for the sake of our unit tests
 
     @Test
-    void shouldReturnAPayCardWhenDataIsSaved(){
+    void shouldReturnAPayCardWhenDataIsSaved() {
 //        get a response entity from the specified url
         ResponseEntity<String> response = restTemplate
-                .withBasicAuth("VictorI","123abcxyz")
-                .getForEntity("/api/v1/paycards/100",String.class);
+                .withBasicAuth("VictorI", "123abcxyz")
+                .getForEntity("/api/v1/paycards/100", String.class);
 //        what do we want to assert, that we have a successful http request , there are multiple ways to check this
 //        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
@@ -36,7 +36,7 @@ class PayVaultApplicationTests {
         DocumentContext documentContext = JsonPath.parse(response.getBody());
         Number id = documentContext.read("$.id");
         assertThat(id).isEqualTo(100);
-        System.out.println( "OUTPUT IS :"+ response.getBody());
+        System.out.println("OUTPUT IS :" + response.getBody());
         // test the amount too
         Double vaultBalance = documentContext.read("$.balance");
 //        assertThat(vaultBalance).isGreaterThan(200);
@@ -46,7 +46,7 @@ class PayVaultApplicationTests {
     }
 
     @Test
-    void shouldNotReturnAPayCardWithInvalidId(){
+    void shouldNotReturnAPayCardWithInvalidId() {
         // we are testing to see that the application Id's are never > 10000
         ResponseEntity<String> response = restTemplate
                 .withBasicAuth("VictorI", "123abcxyz")
@@ -57,12 +57,12 @@ class PayVaultApplicationTests {
     }
 
     @Test
-    void shouldCreateANewPayCard(){
+    void shouldCreateANewPayCard() {
         Double dummyBalance = 500.4;
-        PayCard payCard = new PayCard(null,dummyBalance,"testUser1");
+        PayCard payCard = new PayCard(null, dummyBalance, "testUser1");
         ResponseEntity<Void> voidResponseEntity = restTemplate
-                .withBasicAuth("VictorI","123abcxyz")
-                .postForEntity("/api/v1/paycards/create",payCard, Void.class);
+                .withBasicAuth("VictorI", "123abcxyz")
+                .postForEntity("/api/v1/paycards/create", payCard, Void.class);
         // if creation is successful we expect a 201(CREATED) response
         assertThat(voidResponseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
@@ -70,8 +70,8 @@ class PayVaultApplicationTests {
         URI newPayCardLocation = voidResponseEntity.getHeaders().getLocation();
         // make a get request to that uri
         ResponseEntity<String> getResponse = restTemplate
-                .withBasicAuth("VictorI","123abcxyz")
-                .getForEntity(newPayCardLocation,String.class);
+                .withBasicAuth("VictorI", "123abcxyz")
+                .getForEntity(newPayCardLocation, String.class);
         System.out.println(newPayCardLocation);
         System.out.println(getResponse.getBody());
         assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -84,10 +84,10 @@ class PayVaultApplicationTests {
     }
 
     @Test
-    void shouldReturnAllPayCards(){
+    void shouldReturnAllPayCards() {
         //make a new request
         ResponseEntity<String> response = restTemplate
-                .withBasicAuth("VictorI","123abcxyz")
+                .withBasicAuth("VictorI", "123abcxyz")
                 .getForEntity("/api/v1/paycards/list_paycards", String.class);
         System.out.println(response.getBody());
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -98,7 +98,7 @@ class PayVaultApplicationTests {
     @Test
     void shouldReturnAllCashCardsWhenListIsRequested() {
         ResponseEntity<String> response = restTemplate
-                .withBasicAuth("VictorI","123abcxyz")
+                .withBasicAuth("VictorI", "123abcxyz")
                 .getForEntity("/api/v1/paycards/list_paycards", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
@@ -113,7 +113,7 @@ class PayVaultApplicationTests {
         assertThat(balances).containsExactlyInAnyOrder(123.45, 1.00, 150.00);
     }
 
-//    test for bad auth
+    //    test for bad auth
     @Test
     void shouldNotReturnAPayCardWhenUsingBadCredentials() {
         ResponseEntity<String> response = restTemplate
@@ -126,6 +126,7 @@ class PayVaultApplicationTests {
                 .getForEntity("/api/v1/paycards/99", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
     }
+
     @Test
     void shouldRejectUsersWhoAreNotCardOwners() {
 //        this should enforce RBAC such that if a user doesn't have the PAYCARD-OWNER Role, they should get a 403 forbidden response.
@@ -136,11 +137,11 @@ class PayVaultApplicationTests {
     }
 
     @Test
-    void shouldNotAllowUsersAccessPayCardsTheyDoNotOwn(){
+    void shouldNotAllowUsersAccessPayCardsTheyDoNotOwn() {
 
         ResponseEntity<String> response = restTemplate
-                .withBasicAuth("VictorI","123abcxyz")
-                .getForEntity("/api/v1/paycards/102",String.class); //102 belongs to Ben
+                .withBasicAuth("VictorI", "123abcxyz")
+                .getForEntity("/api/v1/paycards/102", String.class); //102 belongs to Ben
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 
@@ -150,20 +151,20 @@ class PayVaultApplicationTests {
 
     @Test
     @DirtiesContext
-    void shouldUpdateExistingPayCard(){
+    void shouldUpdateExistingPayCard() {
 
-        PayCard payCardToUpdate = new PayCard(null,109.8,null);
+        PayCard payCardToUpdate = new PayCard(null, 109.8, null);
         HttpEntity<PayCard> request = new HttpEntity<>(payCardToUpdate);
 
         ResponseEntity<Void> response = restTemplate
-                .withBasicAuth("VictorI","123abcxyz")
-                .exchange("/api/v1/paycards/101", HttpMethod.PUT,request, Void.class);
+                .withBasicAuth("VictorI", "123abcxyz")
+                .exchange("/api/v1/paycards/101", HttpMethod.PUT, request, Void.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
 
         // Make get call again, to confirm that update was successful
         ResponseEntity<String> getResponse = restTemplate
-                .withBasicAuth("VictorI","123abcxyz")
+                .withBasicAuth("VictorI", "123abcxyz")
                 .getForEntity("/api/v1/paycards/101", String.class);
 
         assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -181,60 +182,72 @@ class PayVaultApplicationTests {
         PayCard unknownCard = new PayCard(null, 19.99, null);
         HttpEntity<PayCard> request = new HttpEntity<>(unknownCard);
         ResponseEntity<Void> response = restTemplate
-                .withBasicAuth("VictorI","123abcxyz")
+                .withBasicAuth("VictorI", "123abcxyz")
                 .exchange("/api/v1/paycards/1010", HttpMethod.PUT, request, Void.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
 
     @Test
-    void  shouldNotUpdatePayCardsOwnedByOtherUser(){
+    void shouldNotUpdatePayCardsOwnedByOtherUser() {
 
-        PayCard bensCard = new PayCard(null,299.9,null);
+        PayCard bensCard = new PayCard(null, 299.9, null);
 
         HttpEntity<PayCard> request = new HttpEntity<>(bensCard);
 
         // use Victor's AUTH for Ben's PayCard url /102
         ResponseEntity<Void> response = restTemplate
-                .withBasicAuth("VictorI","123abcxyz")
-                .exchange("/api/v1/paycards/102",HttpMethod.PUT, request, Void.class);
+                .withBasicAuth("VictorI", "123abcxyz")
+                .exchange("/api/v1/paycards/102", HttpMethod.PUT, request, Void.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 
     }
 
     @Test
-    @DirtiesContext // added to avoid changing state of data for other tests.
-    void shouldDeletePayCard(){
+    @DirtiesContext
+        // added to avoid changing state of data for other tests.
+    void shouldDeleteExistingPayCard() {
 
         ResponseEntity<Void> response = restTemplate
-                .withBasicAuth("VictorI","123abcxyz")
-                .exchange("/api/v1/paycards/101",HttpMethod.DELETE, null, Void.class);
+                .withBasicAuth("VictorI", "123abcxyz")
+                .exchange("/api/v1/paycards/101", HttpMethod.DELETE, null, Void.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
 
         ResponseEntity<String> getResponse = restTemplate
-                .withBasicAuth("VictorI","123abcxyz")
-                .getForEntity("/api/v1/paycards/101",String.class);
+                .withBasicAuth("VictorI", "123abcxyz")
+                .getForEntity("/api/v1/paycards/101", String.class);
 
         assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 
     }
 
     @Test
-    @DirtiesContext // added to avoid changing state of data for other tests.
-    void shouldNotDeletePayCardNotOwnedbyUser(){
+    @DirtiesContext
+        // added to avoid changing state of data for other tests.
+    void shouldNotDeletePayCardNotOwnedbyCustomer() {
 
         ResponseEntity<Void> response = restTemplate
-                .withBasicAuth("VictorI","123abcxyz")
-                .exchange("/api/v1/paycards/102",HttpMethod.DELETE, null, Void.class);
+                .withBasicAuth("amaris", "ama789")
+                .exchange("/api/v1/paycards/102", HttpMethod.DELETE, null, Void.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 
-//        ResponseEntity<String> getResponse = restTemplate
-//                .withBasicAuth("VictorI","123abcxyz")
-//                .getForEntity("/api/v1/paycards/101",String.class);
-//
-//        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        // Validate that the previously existing rec is still in db for the actual owner
+        ResponseEntity<String> getResponse = restTemplate
+                .withBasicAuth("BigBen", "big@ben10")
+                .getForEntity("/api/v1/paycards/102", String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
 
+    }
+
+    @Test
+    void shouldNotDeleteNonExistentPayCard() {
+
+        ResponseEntity<Void> response = restTemplate
+                .withBasicAuth("VictorI", "123abcxyz")
+                .exchange("/api/v1/paycards/9999999", HttpMethod.DELETE, null, Void.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 }
